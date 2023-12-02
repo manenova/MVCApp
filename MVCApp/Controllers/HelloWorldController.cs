89ba128815a4e8c1c6f1818915ff3926ff774cd3
manenova <emmanuel.noriegav@gmail.com>
2023-12-02 13:18:43 -0600

    Add project files.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Get: /HelloWorld/
+     
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+         }
+
+
+    //    //Get: /HelloWorld/Welcome/
+    //    public string Welcome(string name, int numTimes = 1)
+    //    {
+    //        return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+
+    //    }
+    
+    }
+
+}
